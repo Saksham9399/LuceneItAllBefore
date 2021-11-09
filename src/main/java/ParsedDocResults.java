57e72eb98326c2e1a9151337a2504bd058dc1f09
Saksham9399 <saagarwa@tcd.ie>
2021-11-09 20:10:19 +0000

    Updated the Project Structure, added trec eval and datasets

---
+++ src/main/java/ParsedDocResults.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class ParsedDocResults {
+        public ArrayList<ArrayList<String>> fields;
+        public int docCount;
+
+        ParsedDocResults(ArrayList<ArrayList<String>> f, int c){
+                fields = f;
+                docCount = c;
+        }
+
+        //public int compareTo(DocumentScore doc) {
+        //        return Float.compare(doc.score, this.score);
+       // }
+}
+
---
+++ src/main/java/driver_program.java
@@ -0,0 +1,8 @@
+// main function to run the code
+public class driver_program {
+    public static void main(String[] args) throws Exception {
+        Indexer Indexer= new Indexer();
+        Indexer.makeIndex();
+
+    }
+}
