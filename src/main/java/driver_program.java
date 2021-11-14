9cc15b506613361474e139aaf02f56d21744e226
Saksham9399 <saagarwa@tcd.ie>
2021-11-14 19:10:39 +0000

    Added the searcher class as well changed the analyser to english analyser

--- src/main/java/driver_program.java
+++ src/main/java/driver_program.java
@@ -3,6 +3,8 @@ public class driver_program {
     public static void main(String[] args) throws Exception {
         Indexer Indexer= new Indexer();
         Indexer.makeIndex();
+        Searcher Searcher = new Searcher();
+        Searcher.main();
 
     }
 }
